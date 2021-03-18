4dae33d54f7c1dea887eba027b5f447d35308a52
daneng1 <engeldb@gmail.com>
2021-03-17 17:13:01 -0700

    added pop out feature to images

--- src/header.js
+++ src/header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './app.css';
 
 class Header extends React.Component{
   render(){
     return(
-      <h1>Welcome to Horned Beasts</h1>
+      <h1 className="header">Welcome to Horned Beasts</h1>
     );
   }
 }
